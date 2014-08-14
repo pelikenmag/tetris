2f8029250702bac271aa6b7c55b7f9300b532f4f
pelikenmag <pelikenmag@gmail.com>
2014-08-14 16:51:25 +0400

    brain.cpp beautifing

--- brain.cpp
+++ brain.cpp
@@ -5,11 +5,10 @@ void turn_matrix(int *,int );
 
 int main(){
 
-	int i,size=4,m[size*size];
+	int i,size=6,m[size*size];
     for (i=0;i<size*size;i++)
 		m[i]=i;
 
-	//draw_matrix(m2,2);
 
 	draw_matrix(m,size);
 	turn_matrix(m,size);
@@ -17,37 +16,32 @@ int main(){
 	draw_matrix(m,size);
 	return 0;
 }
-
+//Функция поворота матрицы (вправо)
+//Вход: матрица, размер
 void turn_matrix(int *matrix,int n){
+    int i=0,j=0,k,tmp,on=n;
 
-	int i=0,j=0,k,tmp,on=n;
-    //matrix=matrix+n+1;n=n-2;//для теста
     for(;n>1;i=0,j=0){
-        for(;j<n-1;j++){
+        for(;j<n-1;j++){            //цикл прохода по элементам верхней строки матрицы
+                                    //взаимообмен значениями 4-х элементов
+                                    //начиная с [0,j]
             tmp=matrix[j*(on)+n-1-i];
             matrix[j*(on)+n-1-i]=matrix[i*(on)+j];
             matrix[i*(on)+j]=matrix[(n-1-j)*(on)+i];
             matrix[(n-1-j)*(on)+i]=matrix[(n-1-i)*(on)+n-1-j];
             matrix[(n-1-i)*(on)+n-1-j]=tmp;
         }
-
-
-    cout<<n<<endl;
-    matrix=matrix+on+1;n=n-2;
+    matrix=matrix+on+1;n=n-2;//переход к более меньшей подматрице размера N-2
     }
-
 }
-
+//Функция вывода содержимого матрицы в консоль
+//Вход: матрица, размер
 void draw_matrix(int matrix[],int size){
-
 	int i,j;
 	for(i=0;i<size;i++){
 		for(j=0;j<size;j++){
 			cout<<matrix[i*size+j]<<"	";
-
-
 		}
 		cout<<endl;
-
 	}
 }
