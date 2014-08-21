eb56776ee26876815961477adf632e88bdb4d2f9
pelikenmag <pelikenmag@gmail.com>
2014-08-21 21:30:05 +0400

    new class Shape and new functions

--- interface.cpp
+++ interface.cpp
@@ -11,31 +11,135 @@ typedef _mergeresult mergeresult;
 //Variables for Field::Show()
 const int FIELD_AS_TEXT=0;
 const int FIELD_AS_2D=1;
+//Variables for Shape::Generate()
+const int SHAPE_ANGLE=0;
+const int SHAPE_STICK=1;
 
 //Game main matrix class
 class Field{
 
-private:
-int *_field;
-int fn,fm;
+  private:
+    int *_field;
+    int fn,fm;
 
 
-public:
-void Init(int, int);
-mergeresult* Merge(int*,int);
-void Show(int);
-
+  public:
+    void Init(int, int);
+    mergeresult* Merge(int*,int);
+    void Show(int);
 };
 
+//Shapes class
+class Shape{
 
+  private:
 
+    int *shapepointer;
+    int shapesize;
+
+    int anglesize;
+    int *angle;
+
+    int sticksize;
+    int *stick;
+
+ 
+  void ShowShapePointer(){
+    int i,j;
+    if (!shapepointer){
+      std::cout<<"Shapepointer is not initialized"<<endl;
+    }
+    else{
+      for (i=0;i<shapesize;i++){
+        for (j=0;j<shapesize;j++){
+          std::cout<<shapepointer[i*shapesize+j];
+        }
+        std::cout<<endl;
+      }
+    }
+  }
+
+  public:
+    Shape();
+    void Generate(int);
+};
+
+//-----------------------------------
 int main(){
 
-  Field main;
-  main.Init(20,10);
-  main.Show(FIELD_AS_TEXT);
+  //Field main;
+  //main.Init(20,10);
+  //main.Show(FIELD_AS_TEXT);
+  
+  class Shape myshape;
+  myshape.Generate(SHAPE_STICK);
+
+
   return 0;
 }
+//------------------------------------
+/*
+Functions
+*/
+//Generates new shape, deletes an old one
+void Shape::Generate(int shapename){
+  int i;
+  if (!shapepointer)
+    delete shapepointer;
+
+  switch(shapename){
+
+    case SHAPE_ANGLE:
+      shapepointer=new int [anglesize*anglesize];
+      for(int i=0;i<anglesize*anglesize;i++)
+        shapepointer[i]=angle[i];
+      shapesize=anglesize;
+      break;
+    case SHAPE_STICK:
+      shapepointer=new int [sticksize*sticksize];
+      for(int i=0;i<sticksize*sticksize;i++)
+        shapepointer[i]=stick[i];
+      shapesize=sticksize;
+      break;
+    default:
+      break;
+  }
+  ShowShapePointer();
+}
+//Shape default constructor
+Shape::Shape(){
+
+      int i;
+      shapepointer=0;
+      
+      /*
+      Angle
+      { 0,1,
+        1,1 };
+      */
+
+      anglesize=2;
+      angle=new int[anglesize*anglesize];
+      for (i=0;i<anglesize*anglesize;i++)
+        angle[i]=1;
+      angle[0]=0;
+      
+
+      /*
+      Stick
+      { 0,0,0,0,0,
+        0,0,0,0,0,
+        0,0,1,1,0,
+        0,0,1,0,0,
+        0,0,1,0,0 };
+      */
+
+      sticksize=5;
+      stick=new int[sticksize*sticksize];
+      for (i=0;i<sticksize*sticksize;i++)
+        stick[i]=0;
+      stick[12]=stick[13]=stick[17]=stick[22]=1;
+      }
 //Creates game field
 void Field::Init(int n,int m){
   int i,j;
