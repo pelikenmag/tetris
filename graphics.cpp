7e3e0c848720d5174a1bc7791a0b3008dd64b656
pelikenmag <pelikenmag@gmail.com>
2014-08-27 20:15:37 +0400

    small changes in graphics

--- graphics.cpp
+++ graphics.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 using namespace std;
+const int SCREEN_HEIGHT=640;
+const int SCREEN_WIDTH=320;
 
 int main(){
 
+  bool quit=false;
   SDL_Window* gWindow = NULL;
   SDL_Renderer* gRenderer = NULL;
+  SDL_Event event_handle;
   SDL_Init(SDL_INIT_VIDEO);
-  gWindow=SDL_CreateWindow("SDL Test",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED, 640,480,SDL_WINDOW_SHOWN);
+  SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" );
+  gWindow=SDL_CreateWindow("SDL Test",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,\
+   SCREEN_WIDTH,SCREEN_HEIGHT,SDL_WINDOW_SHOWN);
+
   gRenderer=SDL_CreateRenderer(gWindow,-1,SDL_RENDERER_ACCELERATED);
   SDL_SetRenderDrawColor(gRenderer,0xFF,0xFF,0xFF,0xFF);
   SDL_RenderClear( gRenderer );
-  while(1)
-    SDL_RenderPresent(gRenderer);
 
+  while(!quit){
+    
+    while(SDL_PollEvent(&event_handle)!=0){
+      if (event_handle.type == SDL_QUIT){
+
+        quit=true;
+      }
+    }
+
+    SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_WIDTH / 4, SCREEN_WIDTH / 4, SCREEN_WIDTH / 4 };
+    SDL_SetRenderDrawColor( gRenderer, 0xFF, 0x00, 0x00, 0xFF );    
+    SDL_RenderFillRect( gRenderer, &fillRect );
+    //SDL_SetRenderDrawColor(gRenderer,0xFF,0xFF,0x00,0xFF);
+    //SDL_RenderClear( gRenderer );
+    SDL_RenderPresent(gRenderer);
+  }
 
   return 0;
 }
